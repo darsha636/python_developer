4c95e92819ffe65bfd985daa07023975ec385d58
gandhidarsha636-cmyk <gandhidarsha636@gmail.com>
2026-07-14 11:18:30 +0530

    Added details for Shankar, Samhitha, and Darsha

--- sample_code/example.py
+++ sample_code/example.py
@@ -1,7 +1,26 @@
-name2 = "Darsha"
-age2 =20
-city2 = "Vadodara"
+# Shankar's details
+name = "Shankar"
+age = 22
+city = "Indore"
+
+print(name)
+print(age)
+print(city)
+
+# Samhitha's details
+name2 = "Samhitha"
+age2 = 22
+city2 = "Telangana"
 
 print(name2)
 print(age2)
 print(city2)
+
+# Darsha's details
+name3 = "Darsha"
+age3 = 20
+city3 = "Vadodara"
+
+print(name3)
+print(age3)
+print(city3)
